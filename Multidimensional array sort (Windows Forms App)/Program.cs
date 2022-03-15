bc96c2e2eb5c81f6733b8276a695866dfffadb99
Hazem Alkhamisi <65124870+H5misi@users.noreply.github.com>
2022-03-15 19:27:59 +0200

    Code using Console App

---
+++ Multidimensional array sort (Windows Forms App)/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // can be an Input from user, and change their size
+            int[,] arr = { {9, 5, 1},
+                           {6, 4, 8},
+                           {3, 2, 7} };
+
+            int[, ] temp = new int[3, 3];
+
+
+            
+            int max;
+            
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    
+                    for (int k = 0; k < arr.GetLength(0); k++)
+                    {
+                        
+
+                        for (int h = 0; h < arr.GetLength(1); h++)
+                        {
+                            if (arr[i, j] < arr[k, h])
+                            {
+                                max = arr[i, j];
+                                arr[i, j] = arr[k, h];
+                                arr[k, h] = max;
+                            }
+                        }
+                        
+                    }
+                    
+                    
+                    
+                }
+                
+            }
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++ )
+                {
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
